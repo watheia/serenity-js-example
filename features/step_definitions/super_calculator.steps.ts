3b4e7c4ecdef000757baac069a906f18750f87e5
Aaron R Miller <amiller@watheia.dev>
2020-12-06 13:26:05 -0800

    Initial commit

---
+++ features/step_definitions/super_calculator.steps.ts
@@ -0,0 +1,32 @@
+import { Ensure, equals } from '@serenity-js/assertions';
+import { actorCalled, actorInTheSpotlight, engage } from '@serenity-js/core';
+import { Navigate } from '@serenity-js/protractor';
+import { Before, Given, Then, When } from 'cucumber';
+import {
+    Actors,
+    CalculationResult,
+    ConfirmCalculation,
+    EnterLeftSideOperand,
+    EnterRightSideOperand,
+    SelectOperator,
+} from '../support/screenplay';
+
+Before(() => engage(new Actors()));
+
+Given(/(.*) decides to use the Super Calculator/, (actorName: string) =>
+    actorCalled(actorName).attemptsTo(
+        Navigate.to('/protractor-demo/'),
+    ));
+
+When(/(?:he|she|they) (?:adds?) (\d+) and (\d+)/, (leftSideOperand: string, rightSideOperand: string) =>
+    actorInTheSpotlight().attemptsTo(
+        EnterLeftSideOperand.of(leftSideOperand),
+        SelectOperator.withSymbol('+'),
+        EnterRightSideOperand.of(rightSideOperand),
+        ConfirmCalculation(),
+    ));
+
+Then(/(?:he|she|they) should see that the result is (.*)/, (expectedResult: string) =>
+    actorInTheSpotlight().attemptsTo(
+        Ensure.that(CalculationResult(), equals(expectedResult)),
+    ));
